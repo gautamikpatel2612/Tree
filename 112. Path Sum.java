2b24b2e226b4c50f92f8b190105b054ea6d0a0d2
Gautami Patel <91278005+gautamikpatel2612@users.noreply.github.com>
2024-09-11 16:42:09 -0400

    easy

---
+++ 112. Path Sum.java
@@ -0,0 +1,64 @@
+/*
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that 
+  adding up all the values along the path equals targetSum.
+A leaf is a node with no children.
+
+Example 1:
+  Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+  Output: true
+  Explanation: The root-to-leaf path with the target sum is shown.
+Example 2:
+  Input: root = [1,2,3], targetSum = 5
+  Output: false
+  Explanation: There two root-to-leaf paths in the tree:
+  (1 --> 2): The sum is 3.
+  (1 --> 3): The sum is 4.
+  There is no root-to-leaf path with sum = 5.
+Example 3:
+  Input: root = [], targetSum = 0
+  Output: false
+  Explanation: Since the tree is empty, there are no root-to-leaf paths.
+  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return sum(root, 0, targetSum);
+    }
+    
+    private boolean sum(TreeNode root, int curSum, int targetSum){
+        //curSum==0;
+        if(root==null){
+            return false;
+        }
+        curSum = curSum + root.val; //add the sroot values
+
+        if(root.left == null && root.right == null){//leaf node
+            if(curSum == targetSum){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        boolean left = sum(root.left, curSum, targetSum);
+        boolean right = sum(root.right, curSum, targetSum);
+
+        return left || right;
+    }
+}
+//TC: O(N)
+//SC: O(H)
